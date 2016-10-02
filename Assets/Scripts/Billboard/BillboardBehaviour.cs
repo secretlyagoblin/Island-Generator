520b91cfa37cfae5fb3c13d49bc87ad8d41fe963
Chris Welch <christopher.david.welch@gmail.com>
2016-10-02 19:43:39 +1300

    Added some billboard tests

---
+++ Assets/Scripts/Billboard/BillboardBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardBehaviour : MonoBehaviour {
+
+    BillboardController _dad;
+
+    void Awake()
+    {
+        _dad = FindObjectOfType<BillboardController>();
+        _dad.AddSprite(transform);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnEnable()
+    {
+        _dad.BillboardTransformList.Add(transform);
+    }
+
+    void OnDisable()
+    {
+        _dad.BillboardTransformList.Remove(transform);
+    }
+
+    void OnDestroy()
+    {
+        _dad.BillboardTransformList.Remove(transform);
+    }
+}
