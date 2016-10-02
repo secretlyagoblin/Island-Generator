872f03f69415238f671029cbdbeb9818ac76a3bb
Chris Welch <christopher.david.welch@gmail.com>
2016-10-02 20:12:09 +1300

    Started playing with some sprite stuff

--- Assets/Scripts/IslandGen/MapGenerator.cs
+++ Assets/Scripts/IslandGen/MapGenerator.cs
@@ -46,14 +46,14 @@ public class MapGenerator
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                Destroy(transform.GetChild(i).gameObject);
-            }
-            GenerateMap(Size, Size);
-        }
+       // if (Input.GetMouseButtonDown(0))
+       // {
+       //     for (int i = 0; i < transform.childCount; i++)
+       //     {
+       //         Destroy(transform.GetChild(i).gameObject);
+       //     }
+       //     GenerateMap(Size, Size);
+       // }
     }
 
     //Map Gen Functions
