5ecb3685439d5ea5bf73c6b8448c5c132a34dd8c
Chris Welch <christopher.david.welch@gmail.com>
2016-11-13 17:15:28 +1300

    Started on UI work

---
+++ Assets/Scripts/GameObjectBehaviour/RotateInPlace.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateInPlace : MonoBehaviour {
+
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.up, Speed);        
+	
+	}
+}
