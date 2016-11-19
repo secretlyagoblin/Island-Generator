cbce566b96706be43c5b324342e801cd60f8e141
Chris Welch <christopher.david.welch@gmail.com>
2016-11-19 16:02:53 +1300

    LODS working badly

--- Assets/Scripts/IslandGen/InfiniteTerrain.cs
+++ Assets/Scripts/IslandGen/InfiniteTerrain.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class InfiniteTerrain : MonoBehaviour {
 
-    public const float MaxViewDistance = 200;
+    public const float MaxViewDistance = 400;
     public Transform Viewer;
+    public Material TerrainMaterial;
 
     public static Vector2 _viewerPosition;
     int _chunkSize;
@@ -16,6 +18,7 @@ public class InfiniteTerrain : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
+        RNG.Init(DateTime.Now.ToString());
 
         _chunkSize = 241 - 1;
         _chunksVisibleInViewDistance = Mathf.RoundToInt(MaxViewDistance / _chunkSize);
@@ -57,7 +60,7 @@ public class InfiniteTerrain : MonoBehaviour {
                 }
                 else
                 {
-                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, _chunkSize, transform));
+                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, _chunkSize, transform, TerrainMaterial));
                 }
             }
         }
@@ -69,24 +72,79 @@ public class InfiniteTerrain : MonoBehaviour {
         Vector2 _position;
         Bounds _bounds;
 
-        public TerrainChunk(Vector2 coord, int size, Transform transform)
+
+        int _currentLodCount = 4;
+         int _currentLod = 0;
+
+        Mesh[] _lods;
+
+        MeshFilter _filter;
+
+        public TerrainChunk(Vector2 coord, int size, Transform transform, Material terrainMaterial)
         {
+            _currentLodCount = 4;
+            _currentLod = 0;
+
+
+            _lods = new Mesh[4];
+            var mapSize = 240;
+
+
             _position = coord * size;
+
+            for (int i = 0; i < _currentLodCount; i++)
+            {
+                var map = new Map(mapSize, mapSize).PerlinFillMap(3, new Domain(0.3f, 1.8f), new Coord((int)coord.x, (int)coord.y), new Vector2(0.5f, 0.5f), new Vector2(0, 0), 7, 0.5f, 1.87f).Clamp(1, 2f);
+
+                _lods[i] = HeightmeshGenerator.GenerateHeightmeshPatch(map, new MeshLens(new Vector3(size, size, size))).CreateMesh();
+
+                mapSize = (int)(mapSize * 0.5f);
+            }
+
+
+
             _bounds = new Bounds(_position, Vector2.one * size);
             var positionVector3 = new Vector3(_position.x, 0, _position.y);
 
 
 
-            _meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            _meshObject = new GameObject();
+
             _meshObject.transform.parent = transform;
             _meshObject.transform.position = positionVector3;
-            _meshObject.transform.localScale = Vector3.one * size * 0.1f;
+            //_meshObject.transform.localScale = Vector3.one * size * 0.1f;
+
+            var renderer = _meshObject.AddComponent<MeshRenderer>();
+            var material = new Material(terrainMaterial);
+            //material.mainTexture = texture;
+            material.color = new Color(material.color.r + (RNG.Next(-20, 20) * 0.01f), material.color.g, material.color.b + (RNG.Next(-20, 20) * 0.01f));
+            renderer.material = material;
+            _filter = _meshObject.AddComponent<MeshFilter>();
+            var collider = _meshObject.AddComponent<MeshCollider>();
+
+            collider.sharedMesh = _lods[1];
+            _filter.mesh = _lods[_currentLodCount-1];
+            _currentLod = _currentLodCount - 1;
+
+
             SetVisible(false);
         }
 
         public void UpdateTerrainChunk()
         {
             var viewerDistanceFromNearestEdge = Mathf.Sqrt(_bounds.SqrDistance(_viewerPosition));
+
+            var part = Mathf.InverseLerp(0, MaxViewDistance, viewerDistanceFromNearestEdge);
+
+            if(part < 1f)
+            {
+                var lod = (int)(part * _currentLodCount);
+                _filter.mesh = _lods[lod];
+            }
+
+
+
+
             var visible = viewerDistanceFromNearestEdge <= MaxViewDistance;
             SetVisible(visible);
         }
