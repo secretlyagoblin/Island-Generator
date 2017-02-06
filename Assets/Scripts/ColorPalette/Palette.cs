ed02cf9c477d334e71a0d5072e6330a3d4ef28d9
Chris Welch <christopher.david.welch@gmail.com>
2017-02-06 14:23:42 +1300

    Added color palettes

---
+++ Assets/Scripts/ColorPalette/Palette.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[System.Serializable]
+public class ColorPalette: ScriptableObject {
+
+    // Use this for initialization
+    public Gradient GroundColor;
+    public Gradient CliffColor;
+
+    [MenuItem("Assets/Create/Color Palette")]
+    public static void CreateAsset()
+    {
+        Util.CreateAsset<ColorPalette>();
+    }
+
+
+    // Update is called once per frame
+
+}
