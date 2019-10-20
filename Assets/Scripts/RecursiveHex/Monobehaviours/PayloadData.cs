e458bed47b3926a63f536c927e75ea93fca810d7
Chris Welch <chris.welch@aurecongroup.com>
2019-10-20 16:40:20 +1300

    added payload editor for debugging

---
+++ Assets/Scripts/RecursiveHex/Monobehaviours/PayloadData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PayloadData : MonoBehaviour
+{
+    public Dictionary<string, object> KeyValuePairs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
