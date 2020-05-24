2fbb377d65ea5527909a0958fecd9d41815ea38c
Chris Welch <christopher.david.welch@gmail.com>
2020-05-24 16:48:20 +1200

    now loading from disk where possible

--- Assets/Scripts/LevelGen/Monobehaviours/RecursiveHexTest.cs
+++ Assets/Scripts/LevelGen/Monobehaviours/RecursiveHexTest.cs
@@ -21,22 +21,49 @@ namespace WanderingRoad.Procgen.Levelgen
 
         private List<HexGroupVisualiser> _gizmosHexGroups;// = new HexGroupVisualiser(PreviewMesh,)
 
+        private string _savePath = "";
+
         // Start is called before the first frame update
         void Start()
         {
+
+            
             _gizmosHexGroups = new List<HexGroupVisualiser>() { };//HexGroupVisualiser(PreviewMesh);
 
-            //RNG.Init("I'd kill fill zill");
+            RNG.Init("I'd kill fill zill");
             //cool seed "4/27/2020 7:45:28 PM"
             // orphan node to test.. "4/27/2020 5:56:43 PM"
             //RNG.Init("3/15/2020 5:58:48 PM");
             // dead end to test... RNG.Init("5/2/2020 5:04:24 PM");
-            RNG.DateTimeInit();
+            //RNG.DateTimeInit();
             //RecursiveHex.RandomSeedProperties.Disable();
+
+            _savePath = $"{Application.persistentDataPath}/{RNG.CurrentSeed()}";
+
+            var layers = Directory.Exists(_savePath) ? GroupsFromFile() : GenerateGroups();
+
+            var color = RNG.NextColor();
+
+            var chunks = new TerrainChunkCollection(layers, 64, 4, CalculateNoise);
+
+            var terrain = TerrainBuilder.BuildTerrain(chunks);
+
+            //var propsets = getLayers.ConvertAll(x => new RegionProps(x, new PropGen()));
+            //
+            //var allProps = new List<PropData>();
+
+            return;
+
+        }
+
+        private List<HexGroup> GenerateGroups()
+        {
+            Debug.Log("Creating files!");
+
 
             var regionIdentifier = new Func<HexPayload, int>(x => x.Region);
             var codeIdentifier = new Func<HexPayload, int>(x => x.Code);
-            var interiorExterior = new Func<HexPayload, int>(x => x.ConnectionStatus == Connection.NotPresent? 1:2);
+            var interiorExterior = new Func<HexPayload, int>(x => x.ConnectionStatus == Connection.NotPresent ? 1 : 2);
             var connector = new Func<HexPayload, int[]>(x => x.Connections.ToArray());
 
             var setRegionRemapper = new Func<HexPayload, Connection, int[], HexPayload>((x, nodeStatus, connections) =>
@@ -84,44 +111,44 @@ namespace WanderingRoad.Procgen.Levelgen
             var layer2 = layer1;
 
 
-                RandomXY.SetRandomSeed(RNG.NextFloat(-1000, 1000), RNG.NextFloat(-1000, 1000));
+            RandomXY.SetRandomSeed(RNG.NextFloat(-1000, 1000), RNG.NextFloat(-1000, 1000));
 
-                //var splayers = new List<HexGroup>(){layer1
-                //    .Subdivide(4, codeIdentifier)
-                //    .ApplyGraph<Levels.HighLevelConnectivity>(codeIdentifier, connector,true)
-                //    .ForEach(x => new HexPayload(x.Payload) { Region = x.Payload.Code })
-                //    .Subdivide(8, codeIdentifier)
-                //    .ApplyGraph<InterconnectionLogic>(true)
-                //                        .ForEach(x => new HexPayload(x.Payload)
-                //    {
-                //        //Color = x.Payload.ConnectionStatus == Connection.Present ? randomColor : x.Payload.ConnectionStatus == Connection.Critical ? randomColor : randomColorDark//RNG.NextColorBright()
-                //        Color = x.Payload.ConnectionStatus == Connection.NotPresent ? new Color(x.Payload.Height * 0.1f, x.Payload.Height * 0.1f, x.Payload.Height * 0.1f) : new Color(x.Payload.Height * 0.3f, 0.6f, x.Payload.Height * 0.3f),
-                //        Height = (x.Payload.ConnectionStatus == Connection.NotPresent ? x.Payload.Height+3 : 1f) + RandomXY.GetOffset(x.Index.Position3d.x,x.Index.Position3d.z).Distance
-                //    })
-                //.Subdivide(2,codeIdentifier)
-                //.ApplyGraph<TestBed>(codeIdentifier, connector, false)
-                //.ApplyGraph<ApplyBounds>(interiorExterior, connector, false)
-                //.ApplyGraph<PostprocessTerrain>(codeIdentifier, connector, true)
-                //.Subdivide(2, codeIdentifier)
-                //};
+            //var splayers = new List<HexGroup>(){layer1
+            //    .Subdivide(4, codeIdentifier)
+            //    .ApplyGraph<Levels.HighLevelConnectivity>(codeIdentifier, connector,true)
+            //    .ForEach(x => new HexPayload(x.Payload) { Region = x.Payload.Code })
+            //    .Subdivide(8, codeIdentifier)
+            //    .ApplyGraph<InterconnectionLogic>(true)
+            //                        .ForEach(x => new HexPayload(x.Payload)
+            //    {
+            //        //Color = x.Payload.ConnectionStatus == Connection.Present ? randomColor : x.Payload.ConnectionStatus == Connection.Critical ? randomColor : randomColorDark//RNG.NextColorBright()
+            //        Color = x.Payload.ConnectionStatus == Connection.NotPresent ? new Color(x.Payload.Height * 0.1f, x.Payload.Height * 0.1f, x.Payload.Height * 0.1f) : new Color(x.Payload.Height * 0.3f, 0.6f, x.Payload.Height * 0.3f),
+            //        Height = (x.Payload.ConnectionStatus == Connection.NotPresent ? x.Payload.Height+3 : 1f) + RandomXY.GetOffset(x.Index.Position3d.x,x.Index.Position3d.z).Distance
+            //    })
+            //.Subdivide(2,codeIdentifier)
+            //.ApplyGraph<TestBed>(codeIdentifier, connector, false)
+            //.ApplyGraph<ApplyBounds>(interiorExterior, connector, false)
+            //.ApplyGraph<PostprocessTerrain>(codeIdentifier, connector, true)
+            //.Subdivide(2, codeIdentifier)
+            //};
 
-                var splayers = layer1
-                    .Subdivide(4, codeIdentifier)
-                    .ApplyGraph<Levels.HighLevelConnectivity>(codeIdentifier, connector, true)
-                    .ForEach(x => new HexPayload(x.Payload) { Region = x.Payload.Code })
-                    .Subdivide(8, codeIdentifier)
-                    .ApplyGraph<InterconnectionLogic>(false)
-                    .GetSubGroups(x => x.Payload.Region)
-                    //.Select(x => x.Subdivide(2, codeIdentifier))
-                    ;
+            var splayers = layer1
+                .Subdivide(4, codeIdentifier)
+                .ApplyGraph<Levels.HighLevelConnectivity>(codeIdentifier, connector, true)
+                .ForEach(x => new HexPayload(x.Payload) { Region = x.Payload.Code })
+                .Subdivide(8, codeIdentifier)
+                .ApplyGraph<InterconnectionLogic>(false)
+                .GetSubGroups(x => x.Payload.Region)
+                //.Select(x => x.Subdivide(2, codeIdentifier))
+                ;
 
-            var savePath = $"{Application.persistentDataPath}/{RNG.CurrentSeed()}/Chunks";
-           
-           
+            var chunkPath = $"{_savePath}/Chunks";
+
+
 
             foreach (var item in splayers)
             {
-                var path = $"{savePath}/{Guid.NewGuid()}.json";
+                var path = $"{chunkPath}/{Guid.NewGuid()}.json";
                 var info = new System.IO.FileInfo(path);
 
                 if (!info.Exists)
@@ -131,166 +158,14 @@ namespace WanderingRoad.Procgen.Levelgen
                 Debug.Log($"Json written to {path}");
             }
 
+            return splayers;
+        }
 
-                //var splayers = layer1
-                //    .Subdivide(4, codeIdentifier)
-                //    .ApplyGraph<HighLevelConnectivity>(codeIdentifier, connector)
-                //    .ForEach(x => new HexPayload(x.Payload) { Region = x.Payload.Code })
-                //    .Subdivide(8, codeIdentifier)
-                //    .ApplyGraph<TestBed>(codeIdentifier, connector, false)
-                //    //.ApplyGraph<ApplyBounds>(interiorExterior, connector, true)
-                //    .GetSubGroups(x => x.Payload.Region)
-                //    .Select(x => x
-                //        
-                //        //.Subdivide(3, codeIdentifier)
-                //        //.Subdivide(2, codeIdentifier)
-                //        );
+        private List<HexGroup> GroupsFromFile()
+        {
+            Debug.Log("Known level seed! Skipping file creation and loading from disk!");
 
-            var color = RNG.NextColor();
-
-            //var chunks = new TerrainChunkCollection(splayers, 64, 4, CalculateNoise);
-
-            //var terrain = TerrainBuilder.BuildTerrain(chunks);
-
-            var propsets = splayers.ConvertAll(x => new RegionProps(x, new PropGen()));
-
-            var allProps = new List<PropData>();
-
-            foreach (var item in propsets)
-            {
-                item.GetFarProps(allProps);
-
-                foreach (var prop in allProps)
-                {
-                    Instantiate(Prefab, prop.Position, Quaternion.Euler(prop.Rotation, prop.Yaw, 0));
-                }
-            }
-
-
-            //var pts = chunks.GetPositions();
-            //
-            //for (int i = 0; i < pts.Length; i++)
-            //{
-            //    for (int u = 0; u < pts[i].Length; u++)
-            //    {
-            //        var pos = pts[i][u];
-            //
-            //        if (pos.y == 0)
-            //            continue;
-            //        var gob = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //        gob.transform.position = pos;
-            //    }
-            //}
-
-
-                //foreach (var layer in splayers)
-                //{
-                //    var group = new HexGroupVisualiser(PreviewMesh);
-                //
-                //    var randomColor = RNG.SimilarColor(color);
-                //    var randomColorDark = RNG.SimilarColor(randomColor);
-                //
-                //    //layer.ForEach(x => new HexPayload(x.Payload)
-                //    //{
-                //    //    //Color = x.Payload.ConnectionStatus == Connection.Present ? randomColor : x.Payload.ConnectionStatus == Connection.Critical ? randomColor : randomColorDark//RNG.NextColorBright()
-                //    //    Color = x.Payload.ConnectionStatus == Connection.NotPresent ? new Color(x.Payload.Height * 0.1f, x.Payload.Height * 0.1f, x.Payload.Height * 0.1f) : new Color(x.Payload.Height * 0.3f, 0.6f, x.Payload.Height * 0.3f),
-                //    //    Height = (x.Payload.ConnectionStatus == Connection.NotPresent ? x.Payload.Height+3 : 1f) + RandomXY.GetOffset(x.Index.Position3d.x,x.Index.Position3d.z).Distance
-                //    //}); ;
-                //
-                //    layer.Bounds.DrawBounds(Color.white,100f);
-                //
-                //    //Debug.Log(layer.Bounds);
-                //
-                //    layer.ForEach(x => new HexPayload(x.Payload)
-                //    {
-                //        //Color = x.Payload.ConnectionStatus == Connection.Present ? randomColor : x.Payload.ConnectionStatus == Connection.Critical ? randomColor : randomColorDark//RNG.NextColorBright()
-                //        Color = x.Payload.ConnectionStatus == Connection.Present ? randomColor : x.Payload.ConnectionStatus == Connection.Critical ? randomColor : randomColor,//RNG.NextColorBright()
-                //        //Color = x.Payload.ConnectionStatus == Connection.Present ? Color.white : x.Payload.ConnectionStatus == Connection.Critical ? Color.white : Color.black,//RNG.NextColorBright()
-                //        //Color = x.Payload.ConnectionStatus == Connection.NotPresent ? new Color(x.Payload.Height * 0.1f, x.Payload.Height * 0.1f, x.Payload.Height * 0.1f) : new Color(x.Payload.Height * 0.3f, 0.6f, x.Payload.Height * 0.3f),
-                //        //Height = (x.Payload.Height + 1)*5
-                //        Height = Mathf.RoundToInt((x.Payload.Height*4 + (x.Payload.EdgeDistance*1))*15 + (x.Payload.ConnectionStatus == Connection.NotPresent?(3+this.NoiseAtIndex(x) * 20) :0)) 
-                //    });
-                //
-                //
-                //    group.HexGroup = layer;
-                //
-                //    _gizmosHexGroups.Add(group);
-                //}
-
-
-
-
-                   //.Subdivide(2, codeIdentifier)
-                   ////.Subdivide(4, codeIdentifier)
-                   //.ForEach(x => new HexPayload(x.Payload)
-                   //                 {
-                   //    Color = x.Payload.ConnectionStatus == Connection.Present ? Color.white : x.Payload.ConnectionStatus == Connection.Critical ? Color.white : Color.black//RNG.NextColorBright()
-                   //,
-                   //                     Height = RNG.NextFloat(30)
-                   //                 })
-
-                ;
-
-                //var anotherLayer = splayer.MassUpdateHexes(graphe.Finalise(standardRemapper))                    
-                // .Subdivide(5, codeIdentifier)
-                // //.Subdivide(3)
-                //.ForEach(x => new HexPayload(x.Payload)
-                //{
-                //    Color = Color.white//Connection.Present ? Color.white : Color.black//RNG.NextColorBright()
-                //,
-                //    Height = RNG.NextFloat(30)
-                //});
-                //.Subdivide(2, codeIdentifier)
-                //.ForEach(x => new HexPayload()
-                //{
-                //    Color = x.Payload.Color.grayscale + RNG.NextFloat(-0.0f, 0.0f) < 0.7 ? Color.black : Color.white
-                //});
-                //.Subdivide(3)//.Subdivide(3);
-                //.ForEach(x => {
-                //    var a = x.Payload;
-                //    a.Color = RNG.NextColor();
-                //    return a;
-                //}).Subdivide(3).Subdivide(1);
-                ;
-                    //.ForEach((x, i) => new HexPayload() { Code = 1, Height = 0, Color = Color.white })
-                    //.Subdivide();
-                    ;
-
-                //var mesh = splayer.ToMesh();
-                //var (vertices, triangles) = splayer.ToNetwork(x => 0);
-                //
-                //var offset = 0.0f;
-                //
-                //vertices = vertices.Select(x => x + RNG.NextVector3(-offset, offset)+ Vector3.up).ToArray();
-                //
-                //offset = 0.1f;
-                //
-                //for (int t = 0; t < triangles.Length; t+=3)
-                //{
-                //    var localOffset = RNG.NextVector3(-offset, offset);
-                //    var color = RNG.NextColorBright();
-                //    Debug.DrawLine(vertices[triangles[t]]+ localOffset, vertices[triangles[t + 1]] + localOffset, color, 100f);
-                //    Debug.DrawLine(vertices[triangles[t+1]] + localOffset, vertices[triangles[t + 2]] + localOffset, color, 100f);
-                //    Debug.DrawLine(vertices[triangles[t+2]] + localOffset, vertices[triangles[t]] + localOffset, color, 100f);
-                //
-                //}
-                
-                
-                
-                //this.transform.GetComponent<MeshFilter>().sharedMesh = mesh;
-
-
-
-                //needs refactoring
-                //layerfruu.
-                //layerfruu.ToGameObjects(Prefab);
-                //layerfruu.ToGameObjectsBorder(BorderPrefab);
-
-                //this.GetComponent<MeshFilter>().sharedMesh = layer2.ToMesh();
-            
-
-            return;
-
+            return Directory.GetFiles($"{_savePath}/Chunks").Select(x => new HexGroup(JsonUtility.FromJson<SerialisableHexGroup>(File.ReadAllText(x)))).ToList();
         }
 
         private void OnDrawGizmos()
